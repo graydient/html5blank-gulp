c16da3c4fbc7dd34972d724f7332c21fdb5405d8
Miles <mileskoller@gmail.com>
2016-11-16 14:03:38 -0600

    added refills custom page

--- footer.php
+++ footer.php
@@ -1,14 +1,33 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
+			  <div class="footer-logo">
+			    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="Logo image">
+			  </div>
+			  <div class="footer-links">
+			    <ul>
+			      <li><h3>Content</h3></li>
+			      <li><a href="javascript:void(0)">About</a></li>
+			      <li><a href="javascript:void(0)">Contact</a></li>
+			      <li><a href="javascript:void(0)">Products</a></li>
+			    </ul>
+			    <ul>
+			      <li><h3>Follow Us</h3></li>
+			      <li><a href="javascript:void(0)">Facebook</a></li>
+			      <li><a href="javascript:void(0)">Twitter</a></li>
+			      <li><a href="javascript:void(0)">YouTube</a></li>
+			    </ul>
+			    <ul>
+			      <li><h3>Legal</h3></li>
+			      <li><a href="javascript:void(0)">Terms and Conditions</a></li>
+			      <li><a href="javascript:void(0)">Privacy Policy</a></li>
+			    </ul>
+			  </div>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+			  <hr>
 
+			  <p>Disclaimer area lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, nostrum repudiandae saepe.</p>
 			</footer>
+
 			<!-- /footer -->
 
 		</div>
---
+++ templates/template-refill.php
@@ -0,0 +1,53 @@
+<?php /* Template Name: Custom Refill */ get_header(); ?>
+
+	<!-- Start Refills -->
+
+	<div class="device-background">
+	  <div class="device-text">
+	    <h4>Text about App or Device</h4>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, recusandae? Deserunt repellendus.</p>
+	  </div>
+	  <div class="device">
+	    <div class="screen"></div>
+	  </div>
+	</div>
+
+
+	<div class="flex-boxes">
+	  <a href="javascript:void(0)" class="flex-box flex-box-big">
+	    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
+	    <h1 class="flex-title">A Wide Flex Box Item</h1>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nostrum, libero! Laborum distinctio necessitatibus voluptates eaque officiis, unde illo, earum voluptatum rerum, reiciendis ipsa ex dolorem a dicta, maxime aliquam.</p>
+	  </a>
+	  <a href="javascript:void(0)" class="flex-box">
+	    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
+	    <h1 class="flex-title">Flex Box Item</h1>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum molestiae fugiat tenetur fugit atque dignissimos, fugiat natus vitae.</p>
+	  </a>
+	  <a href="javascript:void(0)" class="flex-box">
+	    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
+	    <h1 class="flex-title">A Flex Box Item</h1>
+	    <p>Lorem adipisicing elit. Voluptas consectetur tempora quis nam, officia tenetur blanditiis in illo dolor?</p>
+	  </a>
+	  <a href="javascript:void(0)" class="flex-box flex-box-big">
+	    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
+	    <h1 class="flex-title">Another Wide Item</h1>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quis ipsum, officia, aperiam tenetur dolor molestiae voluptate perferendis dolorem vel ex, unde fugit blanditiis sapiente.</p>
+	  </a>
+	  <a href="javascript:void(0)" class="flex-box">
+	    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
+	    <h1 class="flex-title">Flex Box Item</h1>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo obcaecati in provident illo.</p>
+	  </a>
+	  <a href="javascript:void(0)" class="flex-box">
+	    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="">
+	    <h1 class="flex-title">Last Flex Box Item</h1>
+	    <p>Lorem ipsum dolor sit amet, elit. Rem, illum.</p>
+	  </a>
+	</div>
+
+
+
+	<!-- End Refills -->
+
+<?php get_footer(); ?>
